ca28d6e108873f1a55a90e3504a5e22712a41566
Kimberly Hayes <kshayes78@gmail.com>
2021-12-26 13:11:48 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+//for (let age = 30; age < 40; age ++) {
+   // console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+//function wrapGifts(gifts) {
+   // for (let i = 0; i < gifts.length; i++) {
+      //  console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    //}
+    //return gifts;
+//}
+//wrapGifts(gifts);
+
+//const names = (["Parents", "Pearlie", "Rick"]);
+//const holiday = "Christmas"
+const newArray = [];
+function writeCards(names, holiday) {
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+    }
+    return newArray;
+}
+
+
+function countDown(positiveInteger) {
+while (positiveInteger > -1) {
+    console.log(positiveInteger--);
+    
+}
+ return positiveInteger;
+}
